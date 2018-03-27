3d248524a483cc57a97e4dbdcf4d58e5aae23ea1
Yasir Ibrahim <yasir.ibrahim@gmail.com>
2018-03-27 09:36:16 +0000

    Changed ugettext_lazy to ugettext

--- edx_sga/showanswer.py
+++ edx_sga/showanswer.py
@@ -1,7 +1,7 @@
 """XBlock mixins for showanswer"""
 import logging
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 from xblock.fields import (
     Scope,
     String,
